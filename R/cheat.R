18838e6382ef1096baf38f41ad3e1f30dc5813d3
filozofXV <75808551+filozofXV@users.noreply.github.com>
2022-01-27 13:30:52 +0100

    Update pastebin

--- R/cheat.R
+++ R/cheat.R
@@ -3,5 +3,6 @@ cheat <- function(number) {
          cat("boxplot(rnorm(100))"),
          cat(readLines("https://pastebin.com/raw/pqjVnTBp")),
          cat(readLines("https://pastebin.com/raw/3XhA5ZgJ")),
-         cat(readLines("https://pastebin.com/raw/7L1G2qtH")))
+         cat(readLines("https://pastebin.com/raw/7L1G2qtH", n = 7)))
 }
+
