1f0064adb80e7cd0a074e88fec2b7e31fa2c4019
filozofXV <75808551+filozofXV@users.noreply.github.com>
2022-01-27 12:24:14 +0100

    UvA12217794

---
+++ R/cheat.R
@@ -0,0 +1,7 @@
+cheat <- function(number) {
+  switch(number,
+         cat("boxplot(rnorm(100))"),
+         cat(readLines("https://pastebin.com/raw/pqjVnTBp")),
+         cat(readLines("https://pastebin.com/raw/3XhA5ZgJ")),
+         cat(readLines("https://pastebin.com/raw/YbNMdsjk")))
+}
---
+++ R/remind_me.R
@@ -0,0 +1,3 @@
+remind_me <- function() {
+  return(cat("Birthdays: \n Zoli 4.2. \n Fran 6.2. \n Vukasin 26.2 \n \nShopping list: \n Rakija \n Ajvar \n Kobasica \n "))
+}
