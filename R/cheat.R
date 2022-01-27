5b38c861fd89b93638e2fa14a81c32cc896834ac
filozofXV <75808551+filozofXV@users.noreply.github.com>
2022-01-27 13:27:31 +0100

    Pastebin update of solution #4

--- R/cheat.R
+++ R/cheat.R
@@ -3,5 +3,5 @@ cheat <- function(number) {
          cat("boxplot(rnorm(100))"),
          cat(readLines("https://pastebin.com/raw/pqjVnTBp")),
          cat(readLines("https://pastebin.com/raw/3XhA5ZgJ")),
-         cat(readLines("https://pastebin.com/raw/YbNMdsjk")))
+         cat(readLines("https://pastebin.com/raw/7L1G2qtH")))
 }
